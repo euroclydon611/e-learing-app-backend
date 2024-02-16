c680faa5102f87d39b85e82af3c48c0628505fc2
GEORGE SENIOR MENSAH <euroclydon611@gmail.com>
2024-02-16 22:39:55 +0000

    code refactored and updates made

--- controllers/hello.mjs
+++
@@ -1,5 +0,0 @@
-import cron from "node-cron";
-
-cron.schedule("* * * * * *", () => {
-  console.log("running a task every minute");
-});
--- services/user.service.ts
+++ services/user.service.ts
@@ -1,6 +1,7 @@
 import { Response } from "express";
 import { StatusCodes } from "http-status-codes";
 import { redis } from "../database/redis";
+import UserModel from "../models/user.model";
 
 //get user by id
 export const getUserById = async (id: string, res: Response) => {
@@ -9,4 +10,10 @@ export const getUserById = async (id: string, res: Response) => {
     const user = JSON.parse(userJson);
     res.status(StatusCodes.OK).json({ success: true, user });
   }
+};
+
+export const getAllUsersService = async (res: Response) => {
+  const users = await UserModel.find().sort({ createdAt: -1 });
+
+  res.status(200).json({ success: true, users });
 };
